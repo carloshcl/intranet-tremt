177ecacbff24704761767234881ee484facf2597
Carlos Leite <carlos.hcl1@gmail.com>
2025-09-26 16:01:27 -0400

    Exercicio 31: Criando Tipo Pessoa

--- backend/src/tremt/intranet/content/area.py
+++ backend/src/tremt/intranet/content/area.py
@@ -2,8 +2,10 @@ from plone.dexterity.content import Container
 from plone.supermodel import model
 from zope.interface import implementer
 
+
 class IArea(model.Schema):
     """Definição de uma Área."""
+
 
 @implementer(IArea)
 class Area(Container):
